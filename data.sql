ba5e6058067bf7cc3910d30ebee8f9694e318cd4
muskan2532150 <rg2532150@gmail.com>
2022-10-22 20:24:57 +0530

    add "join table" for visits

--- data.sql
+++ data.sql
@@ -1,6 +1,5 @@
 /* Populate database with sample data. */
 
--- INSERT ANIMALS
 INSERT INTO animals 
 (species_id, owners_id, name, date_of_birth, escape_attempts, neutered, weight_kg) 
 VALUES
@@ -16,7 +15,6 @@ VALUES
 (1, 4, 'Blossom', '1998-10-13', 3, true, 17),
 (1, NULL, 'Ditto', '2022-05-14', 4, true, 22);
 
--- INSERT OWNERS
 INSERT INTO owners 
 (full_name, age)
 VALUES
@@ -27,9 +25,50 @@ VALUES
 ('Dean Winchester',14),
 ('Jodie Whittaker',38);
 
--- INSERT SPECIES
+
 INSERT INTO species 
 (name)
 VALUES
 ('Pokemon'),
 ('Digimon');
+
+INSERT INTO vets 
+(name, age, date_of_graduation)
+VALUES
+('William Tatcher', 45, '2000-04-23'),
+('Maisy Smith', 26, '2019-01-17'),
+('Stephanie Mendez', 64, '1981-05-04'),
+('Jack Harkness', 38, '2008-06-08');
+
+
+INSERT INTO specializations 
+(vet_id, species_id)
+VALUES
+(1, 1),
+(3, 1),
+(3, 2),
+(4, 2);
+
+INSERT INTO visits 
+(animal_id, vet_id, visit_date)
+VALUES
+(1, 1, '2020-05-24'),
+(1, 3, '2020-07-22'),
+(2, 4, '2021-02-02'),
+(3, 2, '2020-01-05'),
+(3, 2, '2020-03-08'),
+(3, 2, '2020-05-14'),
+(4, 3, '2021-05-04'),
+(5, 4, '2021-02-24'),
+(6, 2, '2019-12-21'),
+(6, 1, '2020-08-10'),
+(6, 2, '2021-04-07'),
+(7, 3, '2019-09-29'),
+(8, 4, '2020-10-03'),
+(8, 4, '2020-11-04'),
+(9, 2, '2019-01-24'),
+(9, 2, '2019-05-15'),
+(9, 2, '2020-02-27'),
+(9, 2, '2020-08-03'),
+(10, 3, '2020-05-24'),
+(10, 1, '2021-01-11');
